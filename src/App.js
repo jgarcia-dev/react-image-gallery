1735f6f47b518d28179841a059de73244f5829aa
John Garcia <johngarciadev@gmail.com>
2022-05-27 16:11:49 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
 import { Component } from "react";
-
-import axios from 'axios';
+import { 
+    BrowserRouter,
+    Routes,
+    Route 
+} from "react-router-dom";
 
 import Nav from "./components/Nav";
-import NotFound from "./components/NotFound";
 import SearchForm from "./components/SearchForm";
 import PhotoContainer from "./components/PhotoContainer";
+import PageNotFound from "./components/PageNotFound";
+
+import axios from 'axios';
 import apiKey from "./config";
-
-
 
 class App extends Component {
 
     state = {
         photoURLs: [],
-        query: ""
+        processingFetch: true,
     }
 
-    getPhotos = (query) => {
+    getPhotoURLs = (query) => {
         axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
         .then( response => {
             const photosData = response.data.photos.photo;
-            
+        
             // build photo urls
-            const sizeSuffix = "m";
             const photoURLs = photosData.map( photo => {
-                return `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${sizeSuffix}.jpg`;
+                return `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`;
             });
 
-            return {photoURLs, query};
+            return photoURLs;
         })
-        .then ( ({photoURLs, query}) => {
+        .then ( photoURLs => {
             this.setState({
-                photoURLs,
-                query
+                photoURLs: photoURLs,
+                processingFetch: false
             });
         })
-        .catch(error => {
-            console.log("Error fetching and parsing data");
+        .catch(err => {
+            console.log("Could not fetch data");
         });
     }
 
     render() {
         return (
-            <div className="container">
-                <SearchForm getPhotos={this.getPhotos}/>
-                <Nav getPhotos={this.getPhotos}/>
-                <PhotoContainer
-                    query={this.state.query}
-                    photoURLs={this.state.photoURLs}
-                />
-            </div>
+            <BrowserRouter>
+                <div className="container">
+                    <SearchForm />
+                    <Nav />
+                    <Routes>
+                        <Route path="/" element={null}/>
+                        <Route path="/search/:query" element={<PhotoContainer getPhotoURLs={this.getPhotoURLs} results={this.state} />} />
+                        <Route path="/:query" element={<PhotoContainer getPhotoURLs={this.getPhotoURLs} results={this.state} />} />
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         );
     }
 }
