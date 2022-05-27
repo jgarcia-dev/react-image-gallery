47266787451a466ec6e59c923d500a51c0b5b514
John Garcia <johngarciadev@gmail.com>
2022-05-27 16:12:55 -0700

    Created PageNotFound.js

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+const PageNotFound = () => {
+    return(
+        <div>
+            <h1>404</h1>
+            <h3>Sorry that page is not found</h3>
+        </div>
+    )
+}
+
+export default PageNotFound;
